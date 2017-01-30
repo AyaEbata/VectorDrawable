1148f18ba1516f9c51b5d24f8c0325311d70788d
えばたあや <ayaaaaa.0u0w@gmail.com>
2017-01-31 01:56:24 +0900

    crate TextView + tint

--- app/src/main/java/com/aya/vectordrawable/MainActivity.java
+++ app/src/main/java/com/aya/vectordrawable/MainActivity.java
@@ -1,6 +1,9 @@
 package com.aya.vectordrawable;
 
 import android.os.Bundle;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeTextViewImageTint();
     }
+
+    private void changeTextViewImageTint() {
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_cat_black_24dp);
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(this, R.color.colorAccent));
+    }
+
 }

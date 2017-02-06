f20e2c54db9c28b64ea1358d389da222f5d3e597
えばたあや <ayaaaaa.0u0w@gmail.com>
2017-02-07 02:08:40 +0900

    でけたXD

--- app/src/main/java/com/aya/vectordrawable/MainActivity.java
+++ app/src/main/java/com/aya/vectordrawable/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeTextViewImageTint() {
-        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_cat_black_24dp);
-        drawable = DrawableCompat.wrap(drawable);
-        DrawableCompat.setTint(drawable, ContextCompat.getColor(this, R.color.colorAccent));
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_cat_black_24dp);  // これをそのまま使うと猫のvector画像全部色が変わっちゃう
+        Drawable wrapDrawable = DrawableCompat.wrap(drawable.mutate().getConstantState().newDrawable());  // だからクローンを作ってあげて、
+        DrawableCompat.setTint(wrapDrawable, ContextCompat.getColor(this, R.color.colorAccent));  //  色を変えてあげて、
+        TextView textView = (TextView) findViewById(R.id.text_view_tint);
+        textView.setCompoundDrawablesRelativeWithIntrinsicBounds(wrapDrawable, null, null, null);  // textViewの左側にdrawableつけてあげればできるよっ
     }
 
 }
